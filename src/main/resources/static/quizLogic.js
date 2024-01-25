c2b0f3ee941ead31f44e5b646cc295376bc093fb
emsidhu <emsidhu@cs.sfu.ca>
2024-01-25 02:02:32 +0000

    Wrote questions

---
+++ src/main/resources/static/quizLogic.js
@@ -0,0 +1,115 @@
+let questions = [
+    {
+        query: "What is 1+1?",
+        options: [
+            "0",
+            "1",
+            "2",
+            "3"
+        ],
+        answer: 2,
+        selection: -1
+    },
+    {
+        query: "What is the title of this website?",
+        options: [
+            "Interactive Test",
+            "Science Test",
+            "Interactive Quiz",
+            "Math Quiz"
+        ],
+        answer: 2,
+        selection: -1
+    },
+    {
+        query: "What is the capital of Canada?",
+        options: [
+            "Ottawa",
+            "Canada City",
+            "Toronto",
+            "Vancouver"
+        ],
+        answer: 0,
+        selection: -1
+    },
+    {
+        query: "What is 5*5-10*2?",
+        options: [
+            "-50",
+            "30",
+            "10",
+            "5"
+        ],
+        answer: 3,
+        selection: -1
+    },
+    {
+        query: "What is 2+7?",
+        options: [
+            "5",
+            "7",
+            "9",
+            "2"
+        ],
+        answer: 2,
+        selection: -1
+    },
+    {
+        query: "What is 23+12?",
+        options: [
+            "37",
+            "45",
+            "34",
+            "35"
+        ],
+        answer: 3,
+        selection: -1
+    },
+    {
+        query: "What is 5*12?",
+        options: [
+            "55",
+            "65",
+            "60",
+            "61"
+        ],
+        answer: 2,
+        selection: -1
+    },
+    {
+        query: "What is 19*20/4?",
+        options: [
+            "76",
+            "95",
+            "75",
+            "94"
+        ],
+        answer: 1,
+        selection: -1
+    },
+    {
+        query: "What is 4+25-5+2*3?",
+        options: [
+            "30",
+            "29",
+            "28",
+            "31"
+        ],
+        answer: 0,
+        selection: -1
+    },
+    {
+        query: "What is 19*20/5?",
+        options: [
+            "76",
+            "95",
+            "75",
+            "94"
+        ],
+        answer: 0,
+        selection: -1
+    }
+]
+let curQuestion = 0;
+const NUMQUESTIONS = 10;
+
